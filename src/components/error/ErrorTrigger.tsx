b265c61f906f7576f06b86bad7cea95f9e1d9175
Vlad Shutikov <vshutikov10@gmail.com>
2025-07-15 06:44:36 +0300

    refact: change component to class

--- src/components/error/ErrorTrigger.tsx
+++ src/components/error/ErrorTrigger.tsx
@@ -1,26 +1,28 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const ErrorTrigger: React.FC = () => {
-  const [shouldThrowError, setShouldThrowError] = useState(false);
+class ErrorTrigger extends React.Component {
+  state = { shouldThrowError: false };
 
-  const throwError = () => {
-    setShouldThrowError(true);
+  throwError = () => {
+    this.setState({ shouldThrowError: true });
   };
 
-  if (shouldThrowError) {
-    throw new Error('ERROR SIMULATION');
-  }
+  render(): React.ReactNode {
+    if (this.state.shouldThrowError) {
+      throw new Error('Error from ErrorTrigger!');
+    }
 
-  return (
-    <div className="flex flex-row-reverse py-2 px-4">
-      <button
-        className="w-100 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        onClick={throwError}
-      >
-        Throw Error
-      </button>
-    </div>
-  );
-};
+    return (
+      <div className="flex flex-row-reverse py-2 px-4">
+        <button
+          className="w-100 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+          onClick={this.throwError}
+        >
+          Throw Error
+        </button>
+      </div>
+    );
+  }
+}
 
 export default ErrorTrigger;
